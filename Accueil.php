8fcd2767c8e426dfc678a7cef7151d030224d9ea
Debian <paulbuv@gmail.com>
2026-01-20 09:28:25 +0000

    Suppression du menu resultat

--- Accueil.php
+++ Accueil.php
@@ -15,7 +15,6 @@
                 <li><a href="Concours.php">Concours</a></li>
                 <li><a href="Participants.php">Participants</a></li>
                 <li><a href="Galerie.php">Galerie</a></li>
-                <li><a href="Resultats.php">Résultats</a></li>
                 <li><a href="Admin.php">Administration</a></li>
             </ul>
         </nav>
